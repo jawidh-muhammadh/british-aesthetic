12b3a7c7204b16b2510c1c7369499ed377d7db94
jawidh-muhammadh <jawidhmuhammadh@gmail.com>
2024-10-15 12:03:18 +0530

    changes

---
+++ src/utils/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAKop3tLKPWeD5xfmOPJnQ3eKgorwh9gog",
+  authDomain: "british-aesthetic.firebaseapp.com",
+  projectId: "british-aesthetic",
+  storageBucket: "british-aesthetic.appspot.com",
+  messagingSenderId: "924668563489",
+  appId: "1:924668563489:web:1894e4cb7a9e69aba21360"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,12 @@
 import type { Config } from "tailwindcss";
+import {nextui} from "@nextui-org/react";
 
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {
@@ -12,8 +14,26 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+
+      fontFamily: {
+        poppinssemi: ["var(--font-poppins-semi)"],
+        poppinsbold: ["var(--font-poppins-bold)"],
+        poppinsreg: ["var(--font-poppins-reg)"],
+        poppinsreg5: ["var(--font-poppins-reg5)"],
+        robotolite : ["var(--font-roboto-400)"],
+        robotosemi : ["var(--font-roboto-700)"],
+        robotobold : ["var(--font-roboto-900)"]
+
+    
+        
+   
+      },
+
     },
   },
-  plugins: [],
+  plugins: [
+    nextui(),
+    require('tailwind-scrollbar-hide') , 
+  ],
 };
 export default config;
